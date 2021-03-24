2ababfe222b3f402c32645398b95c441fb193d8c
Rashmi <rashabhi10@gmail.com>
2021-03-25 01:46:36 +0530

    new

---
+++ src/main/java/sst/rash/EmployeeCloudApplication.java
@@ -0,0 +1,13 @@
+package sst.rash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeCloudApplication.class, args);
+	}
+
+}
